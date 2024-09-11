f0bd6496108e87f6e8a78bd6f444246340192e64
shwn <zxz8673@gmail.com>
2024-09-11 16:14:33 +0900

    .

---
+++ C_programming/week2/assignments/code2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int number = 2;
+
+    printf("Enter a number: \n");
+    printf("Sum of digits: %d\n", number);
+    
+    // 홀짝구분
+    printf("Sum is odd\n");
+    printf("Sum is even\n");
+    
+    // 100 미만이면 continue, 처음부터
+    
+    return 0;
+
+}
